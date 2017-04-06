0303b15723fe29121207e323037cf4aec2eaa0a5
YuRi Lee <yurilee9696@gmail.com>
2017-04-06 20:34:18 +0900

    #9 여러 버튼 동작하게 하기

--- app/src/main/java/kr/hs/emirim/wwhhoo0011/friendsimulator/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/wwhhoo0011/friendsimulator/MainActivity.java
@@ -13,6 +13,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     void onClick(View view){
-        Toast.makeText(this, "5~~~~~사카", Toast.LENGTH_SHORT).show();
+        switch(view.getId()){
+            case R.id.button1:
+                Toast.makeText(this, "5~~~~~사카", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button2:
+                Toast.makeText(this, "석순톡 순석톡 카카오톡 에브리바디톡", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button3:
+                Toast.makeText(this, "아침에 모닝 콜필수던 내가", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button5:
+                Toast.makeText(this, "서공예 졸업식 박제", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button4:
+                Toast.makeText(this, "요즘 말야 내가 말야 생각이 많아 댓!댓!댓!", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(this, "다시 선택해주세요!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
     }
+
 }
