25436873aed42e345a3cd569919083452ff528d1
YuRi Lee <yurilee9696@gmail.com>
2017-04-06 20:16:42 +0900

    #8 첫번쨰 버튼 동작하게 하기

--- app/src/main/java/kr/hs/emirim/wwhhoo0011/friendsimulator/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/wwhhoo0011/friendsimulator/MainActivity.java
@@ -1,7 +1,9 @@
 package kr.hs.emirim.wwhhoo0011.friendsimulator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    void onClick(View view){
+        Toast.makeText(this, "5~~~~~사카", Toast.LENGTH_SHORT).show();
     }
 }
