d85f778ab16fa9bcf6c08a769ae40f6f9059f573
YuRi Lee <yurilee9696@gmail.com>
2017-04-10 22:34:17 +0900

    #11 평상시 상태 이미지 보여주기

--- app/src/main/java/kr/hs/emirim/wwhhoo0011/friendsimulator/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/wwhhoo0011/friendsimulator/MainActivity.java
@@ -28,15 +28,15 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "5~~~~~사카", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button2:
-                Log.d(TAG,"버튼2클릭");
+                Log.d(TAG,"버튼2 클릭");
                 Toast.makeText(this, "석순톡 순석톡 카카오톡 에브리바디톡", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button3:
-                Log.d(TAG,"버튼3클릭");
+                Log.d(TAG,"버튼3 클릭");
                 Toast.makeText(this, "아침에 모닝 콜필수던 내가", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button4:
-                Log.e(TAG,"버튼잘못클릭");
+                Log.e(TAG,"버튼 잘못 클릭");
                 Toast.makeText(this, "요즘 말야 내가 말야 생각이 많아 댓!댓!댓!", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button5:
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
             default:
-                Toast.makeText(this, "다시 선택해주세요!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "다시 선택하세요", Toast.LENGTH_SHORT).show();
                 break;
         }
 
